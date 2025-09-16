24659316e0bb0d4344d8c0c69dc9a1fbfbda4596
HephWisdom <williamswisdom018@gmail.com>
2025-09-16 06:30:25 +0000

    update page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SupabaseProvider from "./supabase-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+          <SupabaseProvider>
+          {children}
+        </SupabaseProvider>
       </body>
     </html>
   );
